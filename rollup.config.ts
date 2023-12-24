4bbdf09705f427dd479ed9269b43d44fce9b6843
mo36924 <mo36924@users.noreply.github.com>
2023-12-24 16:10:10 +0000

    feat: change rollup options

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,39 +1,45 @@
 import { readFileSync, readdirSync } from "node:fs";
+import { basename, join, relative } from "node:path";
+import { cwd } from "node:process";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { defineConfig } from "rollup";
 import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
 
-export default defineConfig(async (_) => {
-  const workspaceDir = "packages";
-  const resolve = nodeResolve({ extensions: [".tsx", ".ts"] });
+const workspaceDir = "packages";
+const external = /^[@\w]/;
+const resolve = nodeResolve({ extensions: [".tsx", ".ts"] });
 
-  const input = Object.fromEntries(
-    readdirSync(workspaceDir)
-      .filter((name) => name[0] !== ".")
-      .map((name) => `${workspaceDir}/${name}`)
-      .flatMap((packageDir) =>
-        Object.keys(JSON.parse(readFileSync(`${packageDir}/package.json`, "utf-8")).exports)
-          .map((key) => key.slice(2) || "index")
-          .map((name) => [`${packageDir}/dist/${name}`, `${packageDir}/src/${name}`] as const),
-      ),
-  );
+const input = Object.fromEntries(
+  readdirSync(workspaceDir)
+    .filter((name) => name[0] !== ".")
+    .map((name) => `${workspaceDir}/${name}`)
+    .flatMap((packageDir) =>
+      Object.values<{ [type: string]: string }>(JSON.parse(readFileSync(`${packageDir}/package.json`, "utf-8")).exports)
+        .flatMap((_export) => Object.values(_export))
+        .map((path) => basename(path).replace(/\.(js|cjs|d\.ts)$/, ""))
+        .map((name): [string, string] => [`${packageDir}/dist/${name}`, `${packageDir}/src/${name}`]),
+    ),
+);
 
-  return [
-    {
-      input,
-      output: [
-        { dir: ".", format: "module" },
-        { dir: ".", format: "cjs", entryFileNames: "[name].[format]" },
-      ],
-      external: /^[@\w]/,
-      plugins: [esbuild(), resolve],
+export default defineConfig([
+  {
+    input,
+    output: [
+      { dir: ".", format: "module" },
+      { dir: ".", format: "cjs", entryFileNames: "[name].[format]" },
+    ],
+    external,
+    plugins: [esbuild(), resolve],
+  },
+  {
+    input,
+    output: {
+      dir: ".",
+      format: "module",
+      chunkFileNames: ({ moduleIds }) => `${relative(cwd(), join(moduleIds[0], "..", "..", "dist"))}/[name].d.ts`,
     },
-    {
-      input,
-      output: { dir: ".", format: "module" },
-      external: /^[@\w]/,
-      plugins: [dts(), resolve],
-    },
-  ];
-});
+    external,
+    plugins: [dts(), resolve],
+  },
+]);
