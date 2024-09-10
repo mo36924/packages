85ce091bdc83003974ddf9061e28ee0703a780d9
mo36924 <mo36924@users.noreply.github.com>
2024-09-10 19:09:38 +0900

    chore: update vite-plugin-route-generator options

--- packages/vite-plugin-route-generator/src/index.ts
+++ packages/vite-plugin-route-generator/src/index.ts
@@ -2,7 +2,7 @@ import { watch } from "node:fs";
 import { generateRoutes, Options } from "@mo36924/route-generator";
 import { Plugin } from "vite";
 
-export default (options: Options): Plugin => {
+export default (options: Options = {}): Plugin => {
   const _generateRoutes = generateRoutes.bind(null, options);
   return {
     name: "vite-plugin-route-generator",
