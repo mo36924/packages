96b1474283897010a02b040b282dbe6b24f13bed
mo36924 <mo36924@users.noreply.github.com>
2024-09-16 02:38:12 +0900

    chore: update dependencies and add GraphQL schema support

--- packages/vite-plugin-app/src/index.ts
+++ packages/vite-plugin-app/src/index.ts
@@ -1,9 +1,12 @@
+import { readFileSync } from "node:fs";
 import devServer from "@hono/vite-dev-server";
+import { buildSchema } from "@mo36924/graphql";
 import { precompile as jsxPrecompile } from "@mo36924/jsx-precompile/vite";
 import babel from "@mo36924/vite-plugin-babel";
 import config from "@mo36924/vite-plugin-config";
 import routeGenerator from "@mo36924/vite-plugin-route-generator";
 import ssrBuild from "@mo36924/vite-plugin-ssr-build";
+import { GraphQLSchema } from "graphql";
 import autoImport from "unplugin-auto-import/vite";
 import { Plugin } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -12,6 +15,7 @@ export type Options = {
   server?: string;
   client?: string;
   assets?: string[];
+  schema?: string;
   precompile?: boolean;
 };
 
@@ -19,18 +23,32 @@ export default ({
   server = "src/server/index.tsx",
   client = "src/client/index.tsx",
   assets = ["src/styles/index.css"],
+  schema = "schema.gql",
   precompile,
-}: Options = {}): Plugin[] => [
-  config({ server, client, assets }),
-  routeGenerator(),
-  babel(),
-  ...(precompile ? [jsxPrecompile()] : []),
-  ssrBuild(),
-  devServer({ entry: server }),
-  tsconfigPaths(),
-  autoImport({
-    imports: ["react", { "~/lib/routes": ["match", "Title", "Body", "A", "Router"] }],
-    dirs: ["src/components"],
-    dts: "src/types/imports.d.ts",
-  }),
-];
+}: Options = {}): Plugin[] => {
+  let _schema: string | undefined;
+  let graphQLSchema: GraphQLSchema | undefined;
+
+  try {
+    _schema = readFileSync(schema, "utf-8");
+  } catch {}
+
+  if (_schema) {
+    graphQLSchema = buildSchema(_schema);
+  }
+
+  return [
+    config({ server, client, assets }),
+    routeGenerator(),
+    babel({ schema: graphQLSchema }),
+    ...(precompile ? [jsxPrecompile()] : []),
+    ssrBuild(),
+    devServer({ entry: server }),
+    tsconfigPaths(),
+    autoImport({
+      imports: ["react", { "~/lib/routes": ["match", "Title", "Body", "A", "Router"] }],
+      dirs: ["src/components"],
+      dts: "src/types/imports.d.ts",
+    }),
+  ];
+};
