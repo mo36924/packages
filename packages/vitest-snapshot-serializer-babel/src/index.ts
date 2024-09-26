f988438cd3a353a97e520acf51f08646056a5236
mo36924 <mo36924@users.noreply.github.com>
2024-09-26 17:49:56 +0900

    refactor: optimize vitest-snapshot-serializer-babel

--- packages/vitest-snapshot-serializer-babel/src/index.ts
+++ packages/vitest-snapshot-serializer-babel/src/index.ts
@@ -4,10 +4,6 @@ import { SnapshotSerializer } from "vitest";
 const config = { ...resolveConfig("index.tsx"), filepath: "index.tsx" };
 
 export default {
-  test(value) {
-    return !!value && typeof value?.code === "string" && "ast" in value && "map" in value && "metadata" in value;
-  },
-  serialize(value) {
-    return format(value.code, config);
-  },
+  test: (value) => typeof value?.code === "string" && "ast" in value && "map" in value && "metadata" in value,
+  serialize: (value) => format(value.code, config),
 } satisfies SnapshotSerializer;
