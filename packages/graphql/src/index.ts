4a133fe234e202c967420064c6a31adda6f61577
mo36924 <mo36924@users.noreply.github.com>
2024-09-30 03:00:43 +0900

    chore: refactor graphql exports

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -2,9 +2,13 @@ export * from "./config";
 
 export * from "./data";
 
+export * from "./model";
+
 export * from "./operators";
 
 export * from "./scalars";
+
+export * from "./schema";
 
 export * from "./types";
 
