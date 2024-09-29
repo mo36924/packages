a7f7b915e4b257dbf7cfc466d6fad105c9335641
mo36924 <mo36924@users.noreply.github.com>
2024-09-30 06:03:42 +0900

    refactor: add global gql declaration

--- packages/graphql/src/schema.gql.ts
+++ packages/graphql/src/schema.gql.ts
@@ -1,3 +1,12 @@
 import { getConfig } from "./config";
 
 export const { schema } = getConfig();
+
+declare global {
+  const gql: <
+    T extends { values: any[]; data: any; operation: any } = { values: never[]; data: never; operation: never },
+  >(
+    strings: TemplateStringsArray,
+    ...values: T["values"]
+  ) => { query: string; variables?: Record<string, any>; _data: T["data"]; _operation: T["operation"] };
+}
