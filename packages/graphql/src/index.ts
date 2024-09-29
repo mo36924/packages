87da67c0665f148325b5987ffe9e5788fec959ec
mo36924 <mo36924@users.noreply.github.com>
2024-09-30 01:49:37 +0900

    chore: update graphql package.json

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,3 +1,13 @@
 export * from "./config";
 
+export * from "./data";
+
+export * from "./operators";
+
+export * from "./scalars";
+
+export * from "./types";
+
 export * from "./typescript";
+
+export * from "./utils";
