067f7f6d3a3e55ab2e1f56b3b4c2f31f717d4afa
mo36924 <mo36924@users.noreply.github.com>
2024-11-16 20:14:15 +0900

    feat: add babel-plugin-dead-code-elimination and babel-plugin-replace to babel-preset-jsx

--- packages/babel-preset-jsx/src/index.ts
+++ packages/babel-preset-jsx/src/index.ts
@@ -1,10 +1,12 @@
 import { TransformOptions } from "@babel/core";
+import deadCodeElimination from "@mo36924/babel-plugin-dead-code-elimination";
 import flattenNestedFragments from "@mo36924/babel-plugin-flatten-nested-fragments";
 import graphql, { Options as GraphQLOptions } from "@mo36924/babel-plugin-graphql-tagged-template";
 import injectAssetJsxElements, {
   Options as InjectAssetJsxElementsOptions,
 } from "@mo36924/babel-plugin-inject-asset-jsx-elements";
 import jsxDisplayName from "@mo36924/babel-plugin-jsx-display-name";
+import replace, { Options as ReplaceOptions } from "@mo36924/babel-plugin-replace";
 import replaceJsxElements, { Options as ReplaceJsxElementsOptions } from "@mo36924/babel-plugin-replace-jsx-elements";
 import serverFunction, { Options as ServerFunctionOptions } from "@mo36924/babel-plugin-server-function";
 import { GraphQLSchema } from "graphql";
@@ -50,6 +52,16 @@ export default (
       flattenNestedFragments,
       [jsxDisplayName, development ? undefined : false],
       [graphql, schema ? ({ development, schema } satisfies GraphQLOptions) : false],
+      [
+        replace,
+        {
+          "import.meta.env.PROD": !development,
+          "import.meta.env.DEV": development,
+          "import.meta.env.SSR": !!server,
+          "process.env.NODE_ENV": JSON.stringify(development ? "development" : "production"),
+        } satisfies ReplaceOptions,
+      ],
+      deadCodeElimination,
     ],
   };
 };
