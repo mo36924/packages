66dc3c6c6054dbf1bb9ba1850d0482477afa6d9a
mo36924 <mo36924@users.noreply.github.com>
2024-11-18 02:10:59 +0900

    feat: enhance babel plugin with include/exclude filters and improved transform options handling

--- packages/vite-plugin-babel/src/index.ts
+++ packages/vite-plugin-babel/src/index.ts
@@ -1,12 +1,17 @@
-import { TransformOptions, transformSync } from "@babel/core";
-import { Plugin } from "vite";
+import { TransformOptions as BabelTransformOptions, transformSync } from "@babel/core";
+import { createFilter, FilterPattern, Plugin } from "vite";
+
+type TransformOptions = BabelTransformOptions | null | undefined | void;
 
 export type BabelOptions = {
   enforce?: "pre" | "post";
+  include?: FilterPattern;
+  exclude?: FilterPattern;
   transformOptions: TransformOptions | ((options: { id: string; isBuild: boolean; ssr: boolean }) => TransformOptions);
 };
 
-export const babel = ({ enforce, transformOptions }: BabelOptions): Plugin => {
+export const babel = ({ enforce, include, exclude, transformOptions }: BabelOptions): Plugin => {
+  const filter = createFilter(include, exclude);
   let isBuild = false;
   return {
     name: "vite-plugin-babel",
@@ -15,8 +20,16 @@ export const babel = ({ enforce, transformOptions }: BabelOptions): Plugin => {
       isBuild = command === "build";
     },
     transform(code, id, { ssr = false } = {}) {
+      if (!filter(id)) {
+        return;
+      }
+
       const _transformOptions =
         typeof transformOptions === "function" ? transformOptions({ id, isBuild, ssr }) : transformOptions;
+
+      if (!_transformOptions) {
+        return;
+      }
 
       return transformSync(code, {
         babelrc: false,
