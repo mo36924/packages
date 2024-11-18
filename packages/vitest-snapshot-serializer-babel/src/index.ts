c59551c021bcc072232810394e8a55e4f9b90086
mo36924 <mo36924@users.noreply.github.com>
2024-11-19 02:27:27 +0900

    feat: update formatting options in snapshot serializer for improved error handling

--- packages/vitest-snapshot-serializer-babel/src/index.ts
+++ packages/vitest-snapshot-serializer-babel/src/index.ts
@@ -1,11 +1,23 @@
 import prettier from "@prettier/sync";
 import { SnapshotSerializer } from "vitest";
 
-const { resolveConfig, format } = prettier;
+const options = { ...prettier.resolveConfig("index.tsx"), plugins: [], filepath: "index.tsx" };
 
-const config = { ...resolveConfig("index.tsx"), plugins: [], filepath: "index.tsx" };
+const format = (source: string) => {
+  let _error;
+
+  for (let i = 2; i--; ) {
+    try {
+      return prettier.format(source, options);
+    } catch (error) {
+      _error = error;
+    }
+  }
+
+  throw _error;
+};
 
 export default {
   test: (value) => typeof value?.code === "string" && "map" in value,
-  serialize: (value) => format(value.code, config),
+  serialize: (value) => format(value.code),
 } satisfies SnapshotSerializer;
