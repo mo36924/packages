b2d98b1115098393cf60be9d9bbc826a145e151a
mo36924 <mo36924@users.noreply.github.com>
2024-11-22 07:05:27 +0900

    feat: add bin entry and script for generating drizzle schema

---
+++ packages/graphql-d1/src/bin.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { writeFileSync } from "node:fs";
+import { getSchema } from "@mo36924/graphql";
+import { buildDrizzleSchema } from "./drizzle";
+
+const { config, schema } = getSchema();
+
+if (config.schema && config.drizzle) {
+  const drizzleSchema = buildDrizzleSchema(config.drizzle, schema);
+  writeFileSync(config.drizzle, drizzleSchema);
+}
