7741e01ef00e3a3aee470f9fa68341dce3353c75
mo36924 <mo36924@users.noreply.github.com>
2024-11-23 04:12:26 +0900

    fix: update import statement for runTsc to include file extension

--- packages/graphql-tsc/src/index.ts
+++ packages/graphql-tsc/src/index.ts
@@ -1,6 +1,6 @@
 import { createRequire } from "node:module";
 import { getLanguagePlugin } from "@mo36924/graphql-plugin/volar";
-import { runTsc } from "@volar/typescript/lib/quickstart/runTsc";
+import { runTsc } from "@volar/typescript/lib/quickstart/runTsc.js";
 
 const require = createRequire(import.meta.url);
 const tscPath = require.resolve("typescript/lib/tsc");
