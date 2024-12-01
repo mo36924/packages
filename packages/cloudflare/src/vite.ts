87f8e9a40bfb84055dd774b0b1c1cde602a4f628
mo36924 <mo36924@users.noreply.github.com>
2024-12-02 00:11:07 +0900

    feat: add vite-plugin-preact-debug to Cloudflare package and integrate into Vite configuration

--- packages/cloudflare/src/vite.ts
+++ packages/cloudflare/src/vite.ts
@@ -3,6 +3,7 @@ import cloudflareAdapter from "@hono/vite-dev-server/cloudflare";
 import preset, { Options as PresetOptions } from "@mo36924/babel-preset-jsx";
 import reactRouter from "@mo36924/react-router/vite";
 import babel from "@mo36924/vite-plugin-babel";
+import preactDebug from "@mo36924/vite-plugin-preact-debug";
 import precompileJsx from "@mo36924/vite-plugin-precompile-jsx";
 import { ssr } from "@mo36924/vite-plugin-ssr";
 import preact from "@preact/preset-vite";
@@ -27,5 +28,6 @@ export default ({ input, assets }: Options): Plugin[] => [
   precompileJsx(),
   tsconfigPaths(),
   ...preact({ include: [/\.tsx?$/], exclude: [] }),
+  preactDebug(),
   devServer({ entry: input, adapter: cloudflareAdapter }),
 ];
