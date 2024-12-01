c185d5e4ac54b447f8850b587f69dac37c37ba79
mo36924 <mo36924@users.noreply.github.com>
2024-12-01 20:46:11 +0900

    feat: update react-router integration and remove obsolete plugin

--- packages/cloudflare/src/codegen.ts
+++ packages/cloudflare/src/codegen.ts
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
-import "@mo36924/vite-plugin-react-router/bin";
 import "@mo36924/graphql/dts";
 import "@mo36924/graphql-d1/bin/drizzle";
--- packages/cloudflare/src/vite.ts
+++ packages/cloudflare/src/vite.ts
@@ -1,9 +1,9 @@
 import devServer from "@hono/vite-dev-server";
 import cloudflareAdapter from "@hono/vite-dev-server/cloudflare";
 import preset, { Options as PresetOptions } from "@mo36924/babel-preset-jsx";
+import reactRouter from "@mo36924/react-router/vite";
 import babel from "@mo36924/vite-plugin-babel";
 import precompileJsx from "@mo36924/vite-plugin-precompile-jsx";
-import reactRouter from "@mo36924/vite-plugin-react-router";
 import { ssr } from "@mo36924/vite-plugin-ssr";
 import preact from "@preact/preset-vite";
 import { Plugin } from "vite";
@@ -12,12 +12,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 type Options = {
   input: string;
   assets: string[];
-  importPrefix?: string;
 };
 
-export default ({ input, assets, importPrefix }: Options): Plugin[] => [
+export default ({ input, assets }: Options): Plugin[] => [
   ssr({ input, assets }),
-  reactRouter({ importPrefix }),
+  reactRouter(),
   babel({
     enforce: "pre",
     include: [/\.tsx?$/, /\/@mo36924\//],
