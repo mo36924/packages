2352040cdf39e1805bef429ca6085419ae124c8b
mo36924 <mo36924@users.noreply.github.com>
2024-12-18 12:05:51 +0900

    chore: update babel-dead-code-elimination to version 1.0.8 and remove unused dependencies

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,4 @@
 declare module "@babel/plugin-syntax-jsx";
-declare module "babel-plugin-minify-dead-code-elimination";
 
 declare module "eslint-plugin-no-relative-import-paths" {
   import { ESLint } from "eslint";
